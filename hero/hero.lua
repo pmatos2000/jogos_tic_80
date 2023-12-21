2be25dfa1e82490a6cf3e2e2dccabe2b1ff7ede3
Paulo Matos <pmatos2000@gmail.com>
2023-12-21 00:24:04 -0300

    Exibindo o heroi

--- hero/hero.lua
+++ hero/hero.lua
@@ -6,33 +6,116 @@
 -- version: 0.1
 -- script:  lua
 
-t=0
-x=96
-y=24
 
-function TIC()
+-- Sistema de logs
 
-	if btn(0) then y=y-1 end
-	if btn(1) then y=y+1 end
-	if btn(2) then x=x-1 end
-	if btn(3) then x=x+1 end
 
-	cls(13)
-	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t=t+1
+local logs = {}
+
+local function log_exibir()
+	local y = 4
+	for d=1,#logs do
+		print(logs[d],4,y,4)
+		y=y+8
+	end
 end
 
+local function log_adicionar(log)
+	table.insert(logs, log)
+end
+
+local function log_limpar()
+	logs = {}
+end
+
+function OVR()
+	log_exibir()
+end
+
+local dic_animacao = {
+	HERO_PARADO =  {
+		frames = {
+			{
+				id_sprite = 260,
+				dx = -8,
+				dy = 0,
+			},
+			{
+				id_sprite = 256,
+				dx = 0,
+				dy = 0,
+			},
+			{
+				id_sprite = 259,
+				dx = -8,
+				dy = 8,
+			},
+			{
+				id_sprite = 257,
+				dx = 0,
+				dy = 8,
+			},
+			{
+				id_sprite = 258,
+				dx = 0,
+				dy = 16,
+			},
+		}
+	}
+}
+
+local hero = {
+	vida = 3,
+	pontos = 0,
+	x = 32,
+	y = 48,
+	animacao_nome = "HERO_PARADO",
+	animacao_frame = 0,
+	animacao_virar = false,
+}
+
+
+
+local function desenhar_animacao (animacao_nome, frame, x, y)
+	local animacao = dic_animacao[animacao_nome]
+	if animacao then
+		for i = 1, #animacao.frames do
+			frame = animacao.frames[i]
+			spr(
+				frame.id_sprite,
+				frame.dx + x,
+				frame.dy + y,
+				-1
+			)
+		end
+	else
+		log_adicionar("Animacao nao encontrada: " .. animacao_nome);
+	end
+end
+
+
+
+function TIC()
+	cls()
+	log_limpar()
+	desenhar_animacao(hero.animacao_nome, hero.animacao_frame, hero.x, hero.y)
+end
+
+
+
+
+
 -- <TILES>
--- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
--- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
--- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
--- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
--- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
--- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
--- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 001:3333333333333333333333333333333333333333333333333333333333333333
 -- </TILES>
+
+-- <SPRITES>
+-- 000:0022220002222220222223332222333322223333022222200022220000022000
+-- 001:0044440004444440544444405445444054454440544544405445554004444440
+-- 002:0666666006666660006666000006600000066000000660000006660000066600
+-- 003:0055550000555500005555550005555500005555000005550000005500000000
+-- 004:7777777700077000000220000002200000022000000220000002200000022000
+-- </SPRITES>
 
 -- <WAVES>
 -- 000:00000000ffffffff00000000ffffffff
@@ -49,6 +132,6 @@ end
 -- </TRACKS>
 
 -- <PALETTE>
--- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- 000:1a1c2c5d275db13e5330303041a6f604007dfffffff2c6000000080c040000080c000408000c00180404380404040004
 -- </PALETTE>
 
